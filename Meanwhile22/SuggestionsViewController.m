f292b9ab0e3531cddf1e2d29c20a3ee19981c21a
Michael Hoffman <mhoffman999@gmail.com>
2016-12-05 18:56:51 -0500

    Added elements to SuggestionsVC and Storyboard.

--- Meanwhile22/SuggestionsViewController.m
+++ Meanwhile22/SuggestionsViewController.m
@@ -7,8 +7,19 @@
 //
 
 #import "SuggestionsViewController.h"
+#import "MethodsCache.h"
 
 @interface SuggestionsViewController ()
+
+@property (weak, nonatomic) IBOutlet UIImageView *backgroundImage;
+@property (weak, nonatomic) IBOutlet UIView *pageAccent;
+@property (weak, nonatomic) IBOutlet UIButton *readButton;
+@property (weak, nonatomic) IBOutlet UIButton *watchButton;
+@property (weak, nonatomic) IBOutlet UIButton *previousButton;
+
+- (IBAction)readTapped:(id)sender;
+- (IBAction)watchTapped:(id)sender;
+- (IBAction)previousTapped:(id)sender;
 
 @end
 
@@ -17,11 +28,31 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    self.backgroundImage.image = [UIImage imageNamed:@"paper C lite"];
+    
+    MethodsCache *methods = [MethodsCache new];
+    [methods createViewBorderWidth:2.0 color:[UIColor blackColor] forArray:[self viewArray]];
+    [methods createButtonBorderWidth:2.0 color:[UIColor blackColor] forArray:[self buttonArray]];
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(NSArray *)viewArray
+{
+    NSArray *views = @[self.pageAccent];
+    return views;
+}
+
+-(NSArray *)buttonArray
+{
+    NSArray *buttons = @[self.readButton, self.watchButton, self.previousButton];
+    return buttons;
 }
 
 /*
@@ -33,5 +64,34 @@
     // Pass the selected object to the new view controller.
 }
 */
+
+- (IBAction)readTapped:(id)sender
+{
+    
+}
+
+- (IBAction)watchTapped:(id)sender
+{
+    
+}
+
+- (IBAction)previousTapped:(id)sender
+{
+    [self.navigationController popViewControllerAnimated:YES];
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 @end
